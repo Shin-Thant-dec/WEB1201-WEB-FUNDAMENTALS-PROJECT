063e171c7cbe1c0ef98d00953cb442aabf2c3a36
Shin-Thant-dec <shinthant.busi@gmail.com>
2026-07-28 10:56:57 +0800

    Improvement
    I hope it improves

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const owner_form = document.getElementById('owner-form');
 const firstname_input = document.getElementById('first-name-input');
 const lastname_input = document.getElementById('last-name-input');
 const email_input = document.getElementById('email-input');
@@ -6,7 +6,12 @@ const psw_input = document.getElementById('password-input');
 const repeat_psw_input = document.getElementById('repeat-password-input');
 const error_msg = document.getElementById('error-message');
 
-form.addEventListener('submit', (event) => {
+const ownerSection = document.getElementById('owner');
+const petSection = document.getElementById('pet');
+const loginPage = petSection !== null;
+
+owner_form.addEventListener('submit', (event) => {
+    event.preventDefault();
     let errors = [];
 
     if(firstname_input) {
@@ -16,8 +21,22 @@ form.addEventListener('submit', (event) => {
     }
 
     if (errors.length > 0) {
-        event.preventDefault();
         error_msg.innerText = errors.join('. ');
+    
+        //checks whether if the page is login or sign up
+    } else {
+        if (loginPage) {
+            if (ownerSection) ownerSection.style.display = 'none';
+            if (petSection) {
+                petSection.style.display = 'block';
+            }
+        } else {
+            error_msg.innerText = "Signup successful!"
+            error_msg.style.color = 'green';
+            owner_form.reset();
+
+        }
+
     }
 })
 
@@ -29,11 +48,19 @@ function getSignupFormErrors(firstname, lastname, email, password, repeatpasswor
     if (firstname === '' || firstname == null) {
         errors.push('Firstname is required');
         firstname_input.parentElement.classList.add('incorrect');
+    } else if (!/^[a-zA-Z]+$/.test(firstname)) {
+        errors.push('First name must only contain letters');
+        if(firstname_input) firstname_input.parentElement.classList.add('incorrect');
     }
-    if (lastname === '' || lastname == null) {
-        errors.push('Lastname is required');
-        lastname_input.parentElement.classList.add('incorrect');
+
+    // optional, only runs when last name has numeric
+    if (lastname !== '' && lastname != null) {
+        if (!/^[a-zA-Z]+$/.test(lastname)) {
+            errors.push('Last name must only contain letters');
+            if(lastname_input) lastname_input.parentElement.classList.add('incorrect');
+        }
     }
+
     if (email === '' || email == null) {
         errors.push('Email is required');
        email_input.parentElement.classList.add('incorrect');
@@ -61,7 +88,7 @@ function getSignupFormErrors(firstname, lastname, email, password, repeatpasswor
     return errors;
 }
 
-
+// the last part filter out the input that are empty so that it also works for the login page which only consists of two value
 const allInputs = [firstname_input, lastname_input, email_input, psw_input, repeat_psw_input].filter(input => input != null);
 
 // supposed to be used to remove red border when the input are correct, not working at the moment
@@ -75,3 +102,4 @@ allInputs.forEach(input => {
         });
     }
 });
+
